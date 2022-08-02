fdcdcd7d78c79c79cd5ac2ec988500ebda1deb33
aixia-aiqu <110458839+aixia-aiqu@users.noreply.github.com>
2022-08-02 11:28:43 +0200

    Update init-jupyter.sh

--- init-jupyter.sh
+++ init-jupyter.sh
@@ -4,11 +4,5 @@ apt update
 apt install -y python3 python3-pip
 pip3 install jupyterlab-nvdashboard
 jupyter labextension install jupyterlab-nvdashboard
-echo ------------------
-echo ------jupyterlab intallation completed
-echo ------------------
-echo ------------------ 
-nvidia-smi
-echo ------------------
 
 jupyter lab --ip 0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --port 8888 --allow-root --no-browser &
